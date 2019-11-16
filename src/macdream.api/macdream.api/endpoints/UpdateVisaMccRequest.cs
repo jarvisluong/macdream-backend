853c1c12c4c12a317dd82faa6dd09f58b2b44afe
Chanh Nguyen <chanh.nguyen@medisapiens.fi>
2019-11-16 19:23:14 +0200

    add visa mcc entity

---
+++ src/macdream.api/macdream.api/endpoints/UpdateVisaMccRequest.cs
@@ -0,0 +1,12 @@
+﻿using macdream.api.database;
+using ServiceStack;
+
+namespace macdream.api.endpoints
+{
+	[Route("/visamcc/update", HttpMethods.Put)]
+	public class UpdateVisaMccRequest : IReturn<UpdateVisaMccResponse>
+	{
+        public long VisaMccId { get; set; }
+        public bool isSaving { get; set; }
+    }
+}
